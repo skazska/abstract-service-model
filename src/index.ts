da65e151faa9c31ab28755151d8c3f87f4b27d37
skazska <skazska@gmail.com>
2019-07-17 08:19:01 +0300

    added some dynamodb storage operations

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,37 @@
-import Auth = require('./auth');
-export const auth = Auth;
+// import Auth = require('./auth');
+// export const auth = Auth;
 export * from './auth';
 
 export * from './error';
 
-import Executable = require('./executable');
-export const executable = Executable;
+// import Executable = require('./executable');
+// export const executable = Executable;
 export * from './executable';
+export * from './executable/crud';
+export * from './executable/crud/create';
+export * from './executable/crud/read';
+export * from './executable/crud/update';
+export * from './executable/crud/delete';
 
-
-import IO = require('./i-o');
-export const io = IO;
+// import IO = require('./i-o');
+// export const io = IO;
 export * from './i-o';
 
-import Model = require('./model');
-export const model = Model;
+// import IO = require('./i-o');
+// export const io = IO;
+export * from './i-o/aws/api-gw';
+export * from './i-o/aws/api-gw-proxy';
+
+
+// import Model = require('./model');
+// export const model = Model;
 export * from './model';
 
-import resultDef = require('./result');
-export const result = resultDef;
+// import resultDef = require('./result');
+// export const result = resultDef;
 export * from './result';
 
-import storageDef = require('./storage');
-export const storage = storageDef;
+// import storageDef = require('./storage');
+// export const storage = storageDef;
 export * from './storage';
+export * from './storage/model';
---
+++ src/model/dynamodb.ts
@@ -0,0 +1,1 @@
+//TODO look at: how to interoperate storage executable and io via model. finally json schema should come here
